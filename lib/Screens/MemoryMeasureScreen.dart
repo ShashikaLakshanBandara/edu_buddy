c3ad37b45dcc9ea99a97e1724abdca53abaf8ac0
Shashika Lakshan <shashikalakshanbanadra2000@gmail.com>
2024-05-07 20:01:11 +0530

    Completed memory checked

---
+++ lib/Screens/MemoryMeasureScreen.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+import 'dart:async';
+import 'MemoryTestScreen.dart';
+
+void main() {
+  runApp(const MemoryMeasureScreen());
+}
+
+class MemoryMeasureScreen extends StatefulWidget {
+  const MemoryMeasureScreen({super.key});
+
+  @override
+  State<MemoryMeasureScreen> createState() => _MemoryMeasureScreenState();
+}
+
+class _MemoryMeasureScreenState extends State<MemoryMeasureScreen> {
+  List<String> words = [
+    "Loading...",
+    "Loading...",
+    "Loading...",
+    "Loading...",
+    "Loading...",
+    "Loading..."
+  ];
+  bool showButton = true;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                    "Try to Remember below words as you can"),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.blue, // Set background color here
+                    borderRadius: BorderRadius.circular(15),
+                  ), // Set border radius here
+
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: SizedBox(
+                      height: 500,
+                      width: double.maxFinite,
+                      child: Center(
+                        child: ListView(
+                          children: words
+                              .map((word) => Card(
+                                    child: ListTile(
+                                      title: Text(word),
+                                    ),
+                                  ))
+                              .toList(),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                  child: Spacer(),
+                ),
+                Visibility(
+                  visible: showButton,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.all(15),
+                      backgroundColor: Colors.blue,
+                      foregroundColor: Colors.white,
+                      minimumSize: Size(
+                        MediaQuery.of(context).size.width *
+                            0.9, // Set button width to 50% of screen width
+                        MediaQuery.of(context).size.width *
+                            0.1, // Set button height to 10% of screen width
+                      ),
+                    ),
+                    onPressed: () {
+                      setState(()  {
+                        words = generateWordPairs()
+                            .take(25)
+                            .map((pair) => pair.asPascalCase)
+                            .toList();
+                        showButton = false;
+
+
+                      });
+                      setState(() async {
+                        await Future.delayed(const Duration(seconds: 10));
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => MemoryTestScreen(
+                                words:
+                                words), // Pass words list to MemoryTestScreen
+                          ),
+                        );
+                      });
+
+                    },
+                    child: const Text(
+                      "Start",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Screens/MemoryTestScreen.dart
@@ -0,0 +1,129 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+class MemoryTestScreen extends StatefulWidget {
+  final List<String> words;
+
+  MemoryTestScreen({Key? key, required this.words}) : super(key: key);
+
+  @override
+  State<MemoryTestScreen> createState() => _MemoryTestScreenState();
+}
+
+class _MemoryTestScreenState extends State<MemoryTestScreen> {
+  late List<String> newWords;
+  late List<String> finalWords = [];
+  List<bool> selectedWords = [];
+
+  @override
+  void initState() {
+    super.initState();
+    newWords = generateWordPairs()
+        .take(25)
+        .map((pair) => pair.asPascalCase)
+        .toList();
+    finalWords = List.from(widget.words)..addAll(newWords);
+    finalWords.shuffle(Random());
+    selectedWords = List.filled(finalWords.length, false);
+  }
+
+  void _showSelectedWordCount(BuildContext context) {
+    int count = 0;
+    for (int i = 0; i < finalWords.length; i++) {
+      if (selectedWords[i] && !newWords.contains(finalWords[i])) {
+        count++;
+      }
+    }
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Selected Words'),
+          content: Text('Number of selected words: $count'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              const Text(
+
+                "Select the words which you remembered!",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 20),
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.blue, // Set background color here
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: ListView.builder(
+                      itemCount: finalWords.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          child: CheckboxListTile(
+                            title: Text(finalWords[index]),
+                            value: selectedWords[index],
+                            onChanged: (bool? value) {
+                              setState(() {
+                                selectedWords[index] = value!;
+                              });
+                            },
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+                child: Spacer(),
+              ),
+              ElevatedButton(
+
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.all(15),
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.white,
+                ),
+                onPressed: () {
+                  _showSelectedWordCount(context);
+                },
+                child: const Text(
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16
+                    ),
+                    "Check my memory"
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
